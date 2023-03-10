b8899ba6ec51662921aaadb452501b3f388e257d
Александр Гурьев <arh-tehnik@mail.ru>
2023-03-10 20:50:25 +0300

    Добавьте файлы проекта.

---
+++ 1003/1003.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+template <typename T>  //шаблонны 
+void display(string p, T& value) {
+	cout << p;
+	cin >> value;
+	getline(cout, value);
+	while (cin.fail()) {
+		cin.clear();// восстанавливает поток  ввода после ошибки 
+		cin.ignore()// очищает буфер ввода 
+			cout << p;
+		cin >> value;
+	}
+}
+
+
+int main()
+{
+   
+}
+
